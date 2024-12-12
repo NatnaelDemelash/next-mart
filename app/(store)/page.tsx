81f0afdbb2b6f1f9ad02e2a60ac92312526ef80d
NatnaelDemelash <natidemelash074@gmail.com>
2024-12-12 23:16:36 +0300

    build a helper funtion to get all products

--- app/(store)/page.tsx
+++ app/(store)/page.tsx
@@ -1,4 +1,7 @@
+import { getAllProducts } from '@/sanity/lib/products/getAllProducts';
+
 export default function Home() {
+  const products = getAllProducts();
   return (
     <div>
       <h1>Home Page</h1>
